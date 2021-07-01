e7eeb3cb783b3bf9ad8728db1d4fb5919d2edf0a
Avah Whitehead <avah.r.whitehead@gmail.com>
2021-07-01 16:40:50 +0100

    feat: added interpreter support for extended WHILE

--- test/run/interpreter.test.ts
+++ test/run/interpreter.test.ts
@@ -70,13 +70,77 @@ sum read X {
 } write out
 `;
 
-//<<nil.<nil.nil>>.<nil.nil>
 const prog_many_cons = `
 many_cons read X {
 	X := cons cons nil cons nil nil cons nil nil
 } write X
 `;
 
+const prog_switch = `
+prog read X {
+	switch X {
+		case nil:
+			Y := 1
+		case cons nil nil:
+			Y := 2
+		case cons nil cons nil nil:
+			Y := 3
+		case cons nil cons nil cons nil nil:
+			Y := 4
+		default:
+			Y := 0
+	}
+} write Y
+`;
+
+const prog_switch2 = `
+prog read X {
+	switch X {
+		case nil:
+			Y := 1
+		case <nil.nil>:
+			Y := 2
+		case <nil.<nil.nil>>:
+			Y := 3
+		case <nil.<nil.<nil.nil>>>:
+			Y := 4
+		default:
+			Y := 0
+	}
+} write Y
+`;
+
+const prog_switch3 = `
+prog read X {
+	switch X {
+		case 0:
+			Y := 1
+		case 1:
+			Y := 2
+		case 2:
+			Y := 3
+		case 3:
+			Y := 4
+		default:
+			Y := 0
+	}
+} write Y
+`;
+
+const prog_equals = `
+eq read X {
+	isEq := false;
+	if (hd X) = (tl X) {
+		isEq := true
+	}
+} write isEq
+`;
+
+const prog_equals1 = `
+eq read X {
+	isEq := hd X = tl X
+} write isEq
+`;
 
 describe('Interpreter', function () {
 	describe('prog_ident', function () {
@@ -234,7 +298,7 @@ describe('Interpreter', function () {
 	});
 
 	describe('prog_many_cons', function () {
-		it('any => <<nil.<nil.nil>>.<nil.nil>', function () {
+		it('any => <<nil.<nil.nil>>.<nil.nil>>', function () {
 			let res = Interpreter.parse(prog_many_cons, null);
 			expect(res.success).to.be.true;
 			let interpreter = (res as {success: true,interpreter: Interpreter}).interpreter;
@@ -244,6 +308,117 @@ describe('Interpreter', function () {
 				t(t(null,t(null,null)), t(null,null))
 			);
 		});
+	});
+
+	describe('switch', function () {
+		for (let [name,prog] of [
+			['prog_switch',prog_switch],
+			['prog_switch2',prog_switch2],
+			['prog_switch3',prog_switch3]
+		]) {
+			describe(name, function () {
+				it('0 => 1', function () {
+					let res = Interpreter.parse(prog, null);
+					expect(res.success).to.be.true;
+					let interpreter = (res as {success: true,interpreter: Interpreter}).interpreter;
+					expect(
+						interpreter.run()
+					).to.deep.equal(
+						tn(1)
+					);
+				});
+				it('1 => 2', function () {
+					let res = Interpreter.parse(prog, tn(1));
+					expect(res.success).to.be.true;
+					let interpreter = (res as {success: true,interpreter: Interpreter}).interpreter;
+					expect(
+						interpreter.run()
+					).to.deep.equal(
+						tn(2)
+					);
+				});
+				it('2 => 3', function () {
+					let res = Interpreter.parse(prog, tn(2));
+					expect(res.success).to.be.true;
+					let interpreter = (res as {success: true,interpreter: Interpreter}).interpreter;
+					expect(
+						interpreter.run()
+					).to.deep.equal(
+						tn(3)
+					);
+				});
+				it('3 => 4', function () {
+					let res = Interpreter.parse(prog, tn(3));
+					expect(res.success).to.be.true;
+					let interpreter = (res as {success: true,interpreter: Interpreter}).interpreter;
+					expect(
+						interpreter.run()
+					).to.deep.equal(
+						tn(4)
+					);
+				});
+				it('4 => 0', function () {
+					let res = Interpreter.parse(prog, tn(4));
+					expect(res.success).to.be.true;
+					let interpreter = (res as {success: true,interpreter: Interpreter}).interpreter;
+					expect(
+						interpreter.run()
+					).to.deep.equal(
+						tn(0)
+					);
+				});
+			});
+		}
+	});
+
+	describe('equality', function () {
+		for (let [name,prog] of [
+			['prog_equals',prog_equals],
+			['prog_equals1',prog_equals1]
+		]) {
+			describe(name, function () {
+				it('<nil.nil> => true', function () {
+					let res = Interpreter.parse(prog, t(null, null));
+					expect(res.success).to.be.true;
+					let interpreter = (res as {success: true,interpreter: Interpreter}).interpreter;
+					expect(
+						interpreter.run()
+					).to.deep.equal(
+						tn(1)
+					);
+				});
+				it('<<nil.nil>.<nil.nil>> => true', function () {
+					let res = Interpreter.parse(prog, t(t(null, null), t(null, null)));
+					expect(res.success).to.be.true;
+					let interpreter = (res as {success: true,interpreter: Interpreter}).interpreter;
+					expect(
+						interpreter.run()
+					).to.deep.equal(
+						tn(1)
+					);
+				});
+			});
+			it('<<nil.nil>.nil> => false', function () {
+				let res = Interpreter.parse(prog, t(t(null, null), null));
+				expect(res.success).to.be.true;
+				let interpreter = (res as {success: true,interpreter: Interpreter}).interpreter;
+				expect(
+					interpreter.run()
+				).to.deep.equal(
+					tn(0)
+				);
+			});
+			it('<nil.<nil.nil>> => false', function () {
+				let res = Interpreter.parse(prog, t(null, t(null, null)));
+				expect(res.success).to.be.true;
+				let interpreter = (res as {success: true,interpreter: Interpreter}).interpreter;
+				expect(
+					interpreter.run()
+				).to.deep.equal(
+					tn(0)
+				);
+			});
+		}
 	});
 
 	describe('hd fallback', function () {
@@ -275,6 +450,64 @@ describe('Interpreter', function () {
 			).to.deep.equal(
 				null
 			);
+		});
+	});
+});
+
+describe('Interpreter.treeEquals', function () {
+	describe('equal trees', function () {
+		it(`nil = nil`, function () {
+			expect(
+				Interpreter.treeEquals(null, null)
+			).to.be.true;
+		});
+		it(`<nil.nil> = <nil.nil>`, function () {
+			expect(
+				Interpreter.treeEquals(t(null, null), t(null, null))
+			).to.be.true;
+		});
+		it(`4 = 4`, function () {
+			expect(
+				Interpreter.treeEquals(tn(4), tn(4))
+			).to.be.true;
+		});
+		it(`10 = 10`, function () {
+			expect(
+				Interpreter.treeEquals(tn(10), tn(10))
+			).to.be.true;
+		});
+	});
+
+	describe('different trees', function () {
+		it(`nil != <nil.nil>`, function () {
+			expect(
+				Interpreter.treeEquals(t(null, null), null)
+			).to.be.false;
+		});
+		it(`<nil.nil> != nil`, function () {
+			expect(
+				Interpreter.treeEquals(null, t(null, null))
+			).to.be.false;
+		});
+		it(`4 != 5`, function () {
+			expect(
+				Interpreter.treeEquals(tn(4), tn(5))
+			).to.be.false;
+		});
+		it(`5 != 4`, function () {
+			expect(
+				Interpreter.treeEquals(tn(5), tn(4))
+			).to.be.false;
+		});
+		it(`10 != nil`, function () {
+			expect(
+				Interpreter.treeEquals(tn(10), null)
+			).to.be.false;
+		});
+		it(`nil != 10`, function () {
+			expect(
+				Interpreter.treeEquals(null, tn(10))
+			).to.be.false;
 		});
 	});
 });
